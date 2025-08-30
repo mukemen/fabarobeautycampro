cb39f9582dec5e22e8026abb7f9d312c70840cfb
FAJAR MUKEMEN <43581831+mukemen@users.noreply.github.com>
2025-08-30 17:14:59 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
-const CACHE_NAME = 'cfpro-v2';
+const CACHE_NAME = 'cfpro-v5';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-  );
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
-
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+});
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
   if (url.origin === location.origin) {
